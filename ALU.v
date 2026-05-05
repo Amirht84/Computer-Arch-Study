0397a982944d26a0bdcc0a41323d2b7c3f297cb6
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-05-05 02:38:24 -0700

    edit ALU

--- ALU.v
+++ ALU.v
@@ -1,17 +1,17 @@
 module ALU(A, B, f, Y, Zer);
     input [31:0] A, B;
     input [2:0] f;
-    output reg [31:0] Y;
+    output reg ژ[31:0] Y;
     output zer;
-    always @(a, b, f) begin
+    always @(A, B, f) begin
 	    Y = 32'b0;
 	    case(f) begin
 		    3'b000: Y = A + B;
 		    3'b001: Y = A - B;
 		    3'b010: Y = A & B;
 		    3'b011: Y = A | B;
-		    3'b101: Y = (A < B);
+		    3'b101: Y = {31'b0, (A < B)};
 	    endcase
     end
-    assign zer = ~|Y;
+    assign Zer = ~|Y;
 endmodule
