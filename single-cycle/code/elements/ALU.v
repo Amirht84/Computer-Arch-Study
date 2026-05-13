a32075301b2dc9e4039075040667acf4eae4d7b7
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-05-14 00:59:37 +0330

    debug ALU

--- single-cycle/code/elements/ALU.v
+++ single-cycle/code/elements/ALU.v
@@ -5,14 +5,23 @@ module ALU(A, B, f, Y, zer, lt);
 	output zer;
 	output lt;
 
+	reg [31:0] C;//used temp
+
 	always @(A, B, f) begin
 		Y = 32'b0;
-		case(f) begin
+		case(f)
 			3'b000: Y = A + B;
 			3'b001: Y = A - B;
 			3'b010: Y = A & B;
 			3'b011: Y = A | B;
-			3'b101: Y = {31'b0, (A < B)};
+			3'b101: begin
+				C = A - B;
+				if(C[31] == 1'b0) begin
+					Y = 32'b0;
+				end else begin
+					Y = {31'b0, 1'b1};
+				end
+			end
 		endcase
 	end
 	assign zer = ~|Y;
