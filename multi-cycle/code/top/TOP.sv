fd4e6709debfeeb6b1b29a12bc4a27a63bd5d3bf
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-03 02:09:50 +0330

    edit top module for multi-cycle

--- multi-cycle/code/top/TOP.sv
+++ multi-cycle/code/top/TOP.sv
@@ -1,7 +1,24 @@
-module TOP #(parameter MEM_SPACE = 10, INST_SPACE = 10) (clk);
+module TOP #(parameter MEM_SPACE = 10) (clk);
 	input clk;
-	wire [31:0] InstAdr, MemAdr, InstRD, MemRD, MemWD;
-	wire MemWrite;
-	CPU cpu(.InstAdr(InstAdr), .MemAdr(MemAdr), .InstRD(InstRD), .MemRD(MemRD), .MemWD(MemWD), .MemWrite(MemWrite), .clk(clk));
-	MEM #(.MEM_SPACE(MEM_SPACE), .INST_SPACE(INST_SPACE)) mem(.InstAdr(InstAdr), .MemAdr(MemAdr), .InstRD(InstRD), .MemRD(MemRD), .MemWD(MemWD), .we(MemWrite), .clk(clk));
+	wire [31:0] MemAdr, InstRD, MemRD, MemWD, MemWrite;
+	wire MemWrite, IRWrite;
+	CPU cpu(
+		.MemAdr(MemAdr),
+		.InstRD(InstRD),
+		.MemRD(MemRD),
+		.MemWD(MemWD),
+		.MemWrite(MemWrite),
+		.IRWrite(IRWrite),
+		.clk(clk)
+	);
+
+	MEM #(.MEM_SPACE(MEM_SPACE)) mem(
+		.MemAdr(MemAdr),
+		.InstRD(InstRD),
+		.MemRD(MemRD),
+		.MemWD(MemWD),
+		.MemWrite(MemWrite),
+		.IRWrite(IRWrite),
+		.clk(clk)
+	);
 endmodule
