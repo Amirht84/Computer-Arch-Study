d8e58f280f17cfd83036e46f6beb317d65c44852
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-03 01:48:46 +0330

    add clk pin for main controller and so on

--- multi-cycle/code/controller/MAIN_CNT.sv
+++ multi-cycle/code/controller/MAIN_CNT.sv
@@ -1,7 +1,7 @@
-module MAIN_CNT(OP,AddSrc ,MemWrite ,IRWrite,OldPCWrite,RegWrite,AluSrcA,AluSrcB,ResultSrc,ImmSrc,CondWrite,Write,ALUOp);
+module MAIN_CNT(OP,AddSrc ,MemWrite ,IRWrite,OldPCWrite,RegWrite,AluSrcA,AluSrcB,ResultSrc,ImmSrc,CondWrite,Write,ALUOp, clk);
 	input [6:0] OP;
 	output [1:0] AluSrcA,AluSrcB,ResultSrc,ImmSrc,ALUOp;
-
+	input clk;
 
 endmodule
 	
