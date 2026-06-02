518244b131be36f94c54cb53b5671ec637343b9c
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-02 23:46:25 +0330

    edit MEM top module for multi-cycle

--- multi-cycle/code/top/MEM.sv
+++ multi-cycle/code/top/MEM.sv
@@ -1,7 +1,11 @@
-module MEM #(parameter MEM_SPACE = 10, INST_SPACE = 10) (InstAdr, MemAdr, InstRD, MemRD, MemWD, we, clk);
-	input [31:0] InstAdr, MemAdr, MemWD;
+module MEM #(parameter MEM_SPACE = 10) (MemAdr, InstRD, MemRD, MemWD, MemWrite, IRWrite, clk);
+	input [31:0] MemAdr, MemWD;
+	input MemWrite, IRWrite;
 	output [31:0] InstRD, MemRD;
-	input we ,clk;
-	DATA_MEM #(.DATA_SPACE(MEM_SPACE)) Data(.RDat(MemRD), .Adr(MemAdr), .WDat(MemWD), .clk(clk), .we(we));
-	INST_MEM #(.INST_SPACE(INST_SPACE)) Inst(.RDat(InstRD), .Adr(InstAdr));
+	
+	wire MemOut;
+	REG_EN ir(.Q(MemOut), .D(InstRD), .en(IRWrite), .clk(clk));
+	REG mdr(.Q(MemOut), .D(MemRD), .clk(clk));
+
+	DATA_MEM #(.DATA_SPACE(MEM_SPACE)) Data(.RDat(MemOut), .Adr(MemAdr), .WDat(MemWD), .clk(clk), .we(MemWrite));
 endmodule
