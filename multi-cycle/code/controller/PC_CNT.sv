64f91afca3c33ad182b3bed7a8df6692ad33c18d
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-03 17:22:37 +0330

    change pin name PCSrc to PCWrite

--- multi-cycle/code/controller/PC_CNT.sv
+++ multi-cycle/code/controller/PC_CNT.sv
@@ -1,12 +1,12 @@
-module PC_CNT(CondWrite,Write,Lt_Eq,Not,lt,zer,PCSrc);
+module PC_CNT(CondWrite,Write,Lt_Eq,Not,lt,zer,PCWrite);
 
 	wire Y1,Y2,Y3;
 	input CondWrite,Write,Lt_Eq,Not,lt,zer;
-	output PCSrc;
+	output PCWrite;
 
 	MUX_2IN_1B Mux(.A(zer), .B(lt), .sel(Lt_Eq), .Y(Y1));
 	AND And(.A(Y1) , .B(CondWrite) , .Y(Y2));
 	XOR Xor(.A(Y2) , .B(Not) , .Y(Y3));
-	OR Or(.A(Y3) , .B(Write) , .Y(PCSrc));
+	OR Or(.A(Y3) , .B(Write) , .Y(PCWrite));
 
 endmodule
