2f079067cca034174687706830e1802c4b688692
anita <anitazekavat@gmail.com>
2026-06-03 15:31:55 +0330

    add NANOMEM

---
+++ multi-cycle/code/controller/NANOMEM.sv
@@ -0,0 +1,10 @@
+module NANO_MEM(Write,CondWrite,AdrSrc,MemWrite ,OldPCWrite,IRWrite,RegWrite,ALUSrcA,ALUSrcB,ImmSrc, ALUOp,ResultSrc,NanoIdx);
+    input [3:0] NanoIdx;
+    output [1:0] ALUOp,ALUSrcA,ALUSrcB,ImmSr,ResultSrc;
+    output Write,CondWrite,AdrSrc,MemWrite ,OldPCWrite,IRWrite,RegWrite;
+
+    PAL #(.Rows(12), .Cols(17), .FileName("NANO_MEM.txt")) nano_mem (
+        .Adr(NanoIdx),
+        .Data({Write,CondWrite,AdrSrc,MemWrite ,OldPCWrite,IRWrite,RegWrite,ALUSrcA,ALUSrcB,ImmSrc, ALUOp,ResultSrc})
+    );
+endmodule
