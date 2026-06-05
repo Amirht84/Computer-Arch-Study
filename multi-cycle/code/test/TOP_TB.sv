622c053522e536f14b043640ec21fbce16758888
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-05 17:05:58 +0330

    update test bench to a better version

--- multi-cycle/code/test/TOP_TB.sv
+++ multi-cycle/code/test/TOP_TB.sv
@@ -1,25 +1,31 @@
 module TOP_TB();
+	parameter cycleCountRoof = 10000;
 	integer EndCnt = 0;
-
+	integer CycleCounter = 0;
 	reg clk;
 
 	TOP #(.MEM_SPACE(30)) our_top(clk);
 
 	initial clk = 0;
-	always begin
-		#200;
-		clk = ~clk;
+	always #200 clk = ~clk;
 
-		#10;
+	always @(posedge clk) begin
+		CycleCounter <= CycleCounter + 1;
 
 		if(our_top.cpu.data_path.OldPCOut == 32'd64) begin
-			EndCnt = EndCnt + 0;
+			EndCnt <= EndCnt + 1;
 		end else begin
-			EndCnt = 0;
+			EndCnt <= 0;
 		end
 
 		if(EndCnt >= 6) begin
+			$display("End of the program Reached!");
+			$stop;
+		end
+		if(CycleCounter >= cycleCountRoof) begin
+			$display("CycleCountRoof Reached!");
 			$stop;
 		end
 	end
+
 endmodule
