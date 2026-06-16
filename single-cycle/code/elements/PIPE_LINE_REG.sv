98ec55e40d0846d393413b37dc091dbdbe58d0f7
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-16 19:49:04 +0330

    debug pipeline register

--- single-cycle/code/elements/PIPE_LINE_REG.sv
+++ single-cycle/code/elements/PIPE_LINE_REG.sv
@@ -1,4 +1,4 @@
-module PIPE_LINE_REG #(parameter RegisterCount = 3, parameter InitValue = 32'b0 )(Qs, Ds, iz, en, clk);
+module PIPE_LINE_REG #(parameter RegisterCount = 1, parameter InitValue = 32'b0 )(Qs, Ds, iz, en, clk);
     input [31:0] Ds [0:RegisterCount - 1];
     output [31:0] Qs [0:RegisterCount - 1];
     input iz, en, clk;
