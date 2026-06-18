cb97725286f22af15e45b1ff7220250a9ee0f30f
Amir Hosseini <amir.h.t.1205@gmail.com>
2026-06-18 21:56:55 +0330

    edit cu for pipeline

--- single-cycle/code/controller/CU.sv
+++ single-cycle/code/controller/CU.sv
@@ -1,44 +1,108 @@
-module CU(PCSrc,WSrc,RegWrite,ImmSrc,ALUSrc,ALUfunc,MemWrite,ResultSrc,AddSrc,Func3,Func7,Op,zer,lt);
-	input [6:0] Func7,Op;
+module CU(PCSrcE ,RegWriteW ,ImmSrcD ,ALUSrcE ,ALUFuncE ,MemWriteM ,ResultSrcW ,AddSrcE , Func3, Func7, OpD, zer, lt, IzDE);
+	input [6:0] Func7,OpD;
 	input [2:0] Func3;
-	input zer,lt;
-	output PCSrc,WSrc,RegWrite,ALUSrc,MemWrite,ResultSrc,AddSrc;
-	output [1:0] ImmSrc;
-	output [2:0] ALUfunc;
+	input zer,lt, IzDE;
+	output PCSrcE, RegWriteW, ALUSrcE, MemWriteM , AddSrcE;
+	output [1:0] ResultSrcW, ImmSrcD;
+	output [2:0] ALUFuncE;
 
-	wire [1:0] ALUOp;
-	wire Branch,Jump,Lt_Eq , Not;
 
-	MAIN_CNT MCC(
-		.Op(Op),
-		.ALUOp(ALUOp),
-		.RegWrite(RegWrite),
-		.ALUSrc(ALUSrc), 
-		.Branch(Branch), 
-		.Jump(Jump), 
-		.MemWrite(MemWrite), 
-		.ResultSrc(ResultSrc), 
-		.WSrc(WSrc), 
-		.ImmSrc(ImmSrc), 
-		.AddSrc(AddSrc)
+	////////////	Stage:		Memory Access	#D	////////////
+	wire RegWriteD, AddSrcD, BranchD, JumpD, MemWriteD, ResultSrcD, Lt_EqD, NotD;
+	wire [1:0] ALUOpD;
+	wire [2:0] ALUFuncD;
+
+	MAIN_CNT main_cnt(
+		.Op(OpD),
+		.ALUOp(ALUOpD),
+		.RegWrite(RegWriteD),
+		.ALUSrc(ALUSrcD), 
+		.Branch(BranchD), 
+		.Jump(JumpD),
+		.MemWrite(MemWriteD), 
+		.ResultSrc(ResultSrcD), 
+		.ImmSrc(ImmSrcD), 
+		.AddSrc(AddSrcD)
 	);
-
-	FUNC_CNT FCC(
-		.ALUOp(ALUOp),
+	
+	FUNC_CNT func_cnt(
+		.ALUOp(ALUOpD),
 		.Func3(Func3),
 		.Func7(Func7),
-		.ALUfunc(ALUfunc),
-		.Lt_Eq(Lt_Eq),
+		.ALUFunc(ALUFuncD),
+		.Lt_Eq(Lt_EqD),
 		.Not(Not)
 	);
 
-	PC_CNT PCC(
-		.Branch(Branch),
-		.Jump(Jump),
-		.Lt_Eq(Lt_Eq),
-		.Not(Not),
+	PIPE_LINE_REG #(.RegisterCount(8), .Weadth(1)) de_reg_1(
+		.Ds({RegWriteD, AddSrcD, ALUSrcD, BranchD, JumpD, MemWriteD, NotD, Lt_EqD}),
+		.Qs({RegWriteE, AddSrcE, ALUSrcE, BranchE, JumpE, MemWriteE, NotE, Lt_EqE}),
+		.Clk(Clk),
+		.Iz(IzDE),
+		.En(1'b1)
+	);
+	
+	PIPE_LINE_REG #(.RegisterCount(1), .Weadth(2)) de_reg_2(
+		.Ds({ResultSrcD}),
+		.Qs({ResultSrcE}),
+		.Clk(Clk),
+		.Iz(IzDE),
+		.En(1'b1)
+	);
+
+	PIPE_LINE_REG #(.RegisterCount(8), .Weadth(3)) de_reg_3(
+		.Ds({ALUFuncD}),
+		.Qs({ALUFuncE}),
+		.Clk(Clk),
+		.Iz(IzDE),
+		.En(1'b1)
+	);
+	////////////	Stage:		Memory Access	#E	////////////
+	wire RegWriteE, AddSrcE, BranchE, JumpE, MemWriteE, NotE, Lt_EqE, PCSrcE, ResultSrcE;
+	wire [1:0] ResultSrcE;
+
+	PC_CNT pc_cnt(
+		.Branch(BranchE),
+		.Jump(JumpE),
+		.Lt_Eq(Lt_EqE),
+		.Not(NotE),
 		.lt(lt),
 		.zer(zer),
-		.PCSrc(PCSrc)
+		.PCSrc(PCSrcE)
 	);
+
+	PIPE_LINE_REG #(.RegisterCount(2), .Weadth(1)) em_reg_1(
+		.Ds({RegWriteE, MemWriteE}),
+		.Qs({RegWriteM, MemWriteM}),
+		.Clk(Clk),
+		.Iz(1'b0),
+		.En(1'b1)
+	);
+	
+	PIPE_LINE_REG #(.RegisterCount(1), .Weadth(2)) em_reg_2(
+		.Ds({ResultSrcE}),
+		.Qs({ResultSrcM}),
+		.Clk(Clk),
+		.Iz(1'b0),
+		.En(1'b1)
+	);
+	////////////	Stage:		Memory Access	#M	////////////
+	wire RegWriteM;
+	wire [1:0] ResultSrcM;
+	PIPE_LINE_REG #(.RegisterCount(1), .Weadth(1)) mw_reg_1 (
+		.Ds({RegWriteM}),
+		.Qs({RegWriteW}),
+		.Clk(Clk),
+		.Iz(1'b0),
+		.En(1'b1)
+	);
+	PIPE_LINE_REG #(.RegisterCount(1), .Weadth(2)) mw_reg_2 (
+		.Ds({ResultSrcM}),
+		.Qs({ResultSrcW}),
+		.Clk(Clk),
+		.Iz(1'b0),
+		.En(1'b1)
+	);
+	////////////	Stage:		Memory Access	#W	////////////
+
 endmodule
