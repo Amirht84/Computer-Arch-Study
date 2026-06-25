9a5f98358767a4c7948dca449db5c831d19b6a66
anita <anitazekavat@gmail.com>
2026-06-25 16:52:26 +0330

    debug main cnt . sv

--- pipe-line/code/controller/MAIN_CNT.sv
+++ pipe-line/code/controller/MAIN_CNT.sv
@@ -1,4 +1,4 @@
-module MAIN_CNT(Op, ALUOp, RegWrite, ALUSrc, Branch, Jump, MemWrite, ResultSrc, WSrc, ImmSrc, AddSrc);
+module MAIN_CNT(Op, ALUOp, RegWrite, ALUSrc, Branch, Jump, MemWrite, ResultSrc, ImmSrc, AddSrc);
 
 	/////////// Op Codes ///////////
 	localparam [6:0] rt = 7'd51, bt = 7'd99, it = 7'd19, lw = 7'd3, sw = 7'd35, jal = 7'd111, jalr = 7'd103;
@@ -12,34 +12,33 @@ module MAIN_CNT(Op, ALUOp, RegWrite, ALUSrc, Branch, Jump, MemWrite, ResultSrc, 
 
 
 	input [6:0] Op;
-	output [1:0] ALUOp, ImmSrc;
-	output RegWrite, ALUSrc, Branch, Jump, MemWrite, ResultSrc, WSrc, AddSrc;
-
-	reg [11:0] OutPut;
+	output [1:0] ALUOp, ImmSrc, ResultSrc;
+	output RegWrite, ALUSrc, Branch, Jump, MemWrite, AddSrc;
+	logic [11:0] OutPut;
 
 	always @(Op) begin
 
 		OutPut = 12'b0;
 
 		case(Op)
-			rt:	OutPut =		{2'b10 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , _2bdd , _1bd };
+			rt:	OutPut =		{2'b10 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 2'b0  , _2bdd , _1bd };
 			
-			bt:	OutPut =		{2'b01 , 1'b0 , 1'b0 , 1'b1 , 1'b0 , 1'b0 , _1bd , _1bd , 2'b10 , 1'b0 };
+			bt:	OutPut =		{2'b01 , 1'b0 , 1'b0 , 1'b1 , 1'b0 , 1'b0 , _2bd , 2'b10 , 1'b0 };
 
-			it:	OutPut =		{2'b11 , 1'b1 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 1'b0 , 2'b00 , _1bd };
+			it:	OutPut =		{2'b11 , 1'b1 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 2'b0 , 2'b00 , _1bd };
 
-			lw:	OutPut =		{2'b00 , 1'b1 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 1'b1 , 1'b0 , 2'b00 , _1bd };
+			lw:	OutPut =		{2'b00 , 1'b1 , 1'b1 , 1'b0 , 1'b0 , 1'b0 , 2'b01 , 2'b00 , _1bd };
 
-			sw:	OutPut =		{2'b00 , 1'b0 , 1'b1 , 1'b0 , 1'b0 , 1'b1 , _1bd , _1bd , 2'b01 , _1bd };
+			sw:	OutPut =		{2'b00 , 1'b0 , 1'b1 , 1'b0 , 1'b0 , 1'b1 , _2bd ,2'b01 , _1bd };
 
-			jal:	OutPut =		{_2bdd , 1'b1 , _1bd , _1bd , 1'b1 , 1'b0 , _1bd , 1'b1 , 2'b11 , 1'b0 };
+			jal:	OutPut =		{_2bdd , 1'b1 , _1bd , _1bd , 1'b1 , 1'b0 , 2'b10 , 2'b11 , 1'b0 };
 
-			jalr:	OutPut =		{_2bdd , 1'b1 , _1bd , _1bd , 1'b1 , 1'b0 , _1bd , 1'b1 , 2'b11 , 1'b1 };
+			jalr:	OutPut =		{_2bdd , 1'b1 , _1bd , _1bd , 1'b1 , 1'b0 , 2'b10 , 2'b11 , 1'b1 };
 
 		endcase
 
 	end
 	
-	assign {ALUOp, RegWrite, ALUSrc, Branch, Jump, MemWrite, ResultSrc, WSrc, ImmSrc, AddSrc} = OutPut;
+	assign {ALUOp, RegWrite, ALUSrc, Branch, Jump, MemWrite, ResultSrc, ImmSrc, AddSrc} = OutPut;
 
 endmodule
